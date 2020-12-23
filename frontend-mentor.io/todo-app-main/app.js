40f5c905690b6cfc9344315f48861bd29d509032
whitegod <kmbagwu12@gmail.com>
2020-12-23 13:37:40 +0100

    Completed App

--- frontend-mentor.io/todo-app-main/app.js
+++ frontend-mentor.io/todo-app-main/app.js
@@ -50,10 +50,31 @@ const TODOController = (function () {
       // 3) add new todo to data structure
       data.allItems.todos.push(newItem);
     },
+
+    getCompleted: function () {
+      return data.allItems.todos.filter((todo) => {
+        if (todo.status) return todo;
+      });
+    },
+
+    getActive: function () {
+      return data.allItems.todos.filter((todo) => {
+        if (!todo.status) return todo;
+      });
+    },
+
     getTodo: function () {
       return {
         todos: data.allItems.todos,
       };
+    },
+
+    clearCompleted: function (todo) {
+      let index = data.allItems.todos.indexOf(todo);
+
+      if (index < 0) return;
+
+      data.allItems.todos.splice(index, 1);
     },
   };
 })();
@@ -70,7 +91,19 @@ const UIcontroller = (function () {
     clearCompletedBtn: ".clear-completed",
     active: "#active",
     active2: "#active-2",
+    completedBtn: "#completed",
+    allBtn: "#all",
     count: ".count",
+    darkMode: ".theme-dark",
+    lightMode: ".theme-light",
+    body: "body",
+    wrapperImg: ".wrapper__img",
+    containerForm: ".container__form",
+    containerTodo: ".container__todos",
+    filters: ".filters",
+    link2: ".links--2",
+    clearCompletedBtn: ".clear-completed",
+    attribution: ".attribution",
   };
 
   // Adds draggable  functionality with animated swapping
@@ -146,6 +179,8 @@ const UIcontroller = (function () {
         numOfItems <= 1
           ? `${numOfItems} item left`
           : `${numOfItems} items left`;
+
+      return numOfItems;
     },
 
     getDomString: function () {
@@ -170,6 +205,97 @@ const controller = (function (TodoCtrl, UICtrl) {
         updateStatus();
       }
     });
+
+    document.querySelectorAll(dom.allBtn).forEach((el) => {
+      el.addEventListener("click", (e) => {
+        let allTodos, numOfItemsLeft;
+        allTodos = TodoCtrl.getTodo().todos;
+        numOfItemsLeft = UICtrl.itemsLeft(allTodos);
+
+        document.querySelector(dom.list).innerHTML = "";
+        showAll(allTodos);
+        addCompletedClass();
+        updateStatus();
+        document.querySelector(dom.count).textContent =
+          numOfItemsLeft <= 1
+            ? `${numOfItemsLeft} item left `
+            : `${numOfItemsLeft} items left `;
+        e.stopPropagation();
+      });
+    });
+
+    document.querySelectorAll(dom.completedBtn).forEach((el) => {
+      el.addEventListener("click", (e) => {
+        const completedTodos = TodoCtrl.getCompleted();
+        document.querySelector(dom.list).innerHTML = "";
+        showAll(completedTodos);
+        addCompletedClass();
+        updateStatus();
+        document.querySelector(dom.count).textContent =
+          completedTodos.length <= 1
+            ? `${completedTodos.length} item completed `
+            : `${completedTodos.length} items Completed `;
+        e.stopPropagation();
+      });
+    });
+
+    document.querySelectorAll(`${dom.active || dom.active2}`).forEach((el) => {
+      el.addEventListener("click", (e) => {
+        const activeTodos = TodoCtrl.getActive();
+        document.querySelector(dom.list).innerHTML = "";
+        showAll(activeTodos);
+        addCompletedClass();
+        updateStatus();
+        document.querySelector(dom.count).textContent =
+          activeTodos.length <= 1
+            ? `${activeTodos.length} item left `
+            : `${activeTodos.length} items left `;
+        e.stopPropagation();
+      });
+    });
+
+    document
+      .querySelector(dom.clearCompletedBtn)
+      .addEventListener("click", () => {
+        const completedTodos = TodoCtrl.getCompleted();
+        completedTodos.forEach((todo) => {
+          TodoCtrl.clearCompleted(todo);
+          document.querySelector(dom.list).innerHTML = "";
+          displayTodos();
+          addCompletedClass();
+          updateStatus();
+        });
+      });
+
+    document.querySelector(dom.lightMode).addEventListener("click", () => {
+      document.querySelector(dom.lightMode).classList.toggle("theme-dark");
+
+      if (
+        document.querySelector(dom.lightMode).classList.contains("theme-dark")
+      ) {
+        document.querySelector(dom.lightMode).querySelector("img").src =
+          "images/icon-sun.svg";
+        document.querySelector(dom.body).classList.add("dark");
+        document.querySelector(dom.wrapperImg).classList.add("dark");
+        document.querySelector(dom.containerForm).classList.add("dark");
+        document.querySelector(dom.containerTodo).classList.add("dark");
+        document.querySelector(dom.filters).classList.add("dark");
+        document.querySelector(dom.clearCompletedBtn).classList.add("dark");
+        document.querySelector(dom.link2).classList.add("dark");
+        document.querySelector(dom.attribution).classList.add("dark");
+      } else {
+        document.querySelector(dom.lightMode).querySelector("img").src =
+          "images/icon-moon.svg";
+        document.querySelector(dom.body).classList.remove("dark");
+        document.querySelector(dom.wrapperImg).classList.remove("dark");
+        document.querySelector(dom.containerForm).classList.remove("dark");
+        document.querySelector(dom.containerTodo).classList.remove("dark");
+        document.querySelector(dom.filters).classList.remove("dark");
+        document.querySelector(dom.clearCompletedBtn).classList.remove("dark");
+        document.querySelector(dom.link2).classList.remove("dark");
+        document.querySelector(dom.attribution).classList.remove("dark");
+      }
+    });
   };
 
   const displayTodos = function () {
@@ -190,6 +316,7 @@ const controller = (function (TodoCtrl, UICtrl) {
   const ctrlAddItem = function () {
     // 1) Get user input
     const todo = UICtrl.getInput();
+
     // 2) Add item to Todo controller
     TodoCtrl.addItem(todo.userInput, false);
     // 3) Add item to UI
@@ -208,23 +335,29 @@ const controller = (function (TodoCtrl, UICtrl) {
       .querySelectorAll(UICtrl.getDomString().checkboxes)
       .forEach((checkbox) => {
         checkbox.addEventListener("click", () => {
+          // Get Id
           const id = Number(
             checkbox.parentElement.parentElement.getAttribute("id")
           );
 
+          // 1) Get Todo
+          const todo = TodoCtrl.getTodo();
+
           if (checkbox.checked) {
             console.log(id);
-            // 1) Get Todo
-            const todo = TodoCtrl.getTodo();
+
             // 2) Update status
-            todo.todos[id].updateStatus(true);
-            console.log(todo.todos[id]);
+            todo.todos.forEach((todo) => {
+              if (todo.id === id) todo.updateStatus(true);
+            });
 
             // 3) Update Count
             UICtrl.itemsLeft(TodoCtrl.getTodo().todos);
           } else {
             // 1) Update status
-            TodoCtrl.getTodo().todos[id].updateStatus(false);
+            todo.todos.forEach((todo) => {
+              if (todo.id === id) todo.updateStatus(false);
+            });
             console.log(TodoCtrl.getTodo().todos[id]);
 
             // 2) Update count
@@ -232,6 +365,12 @@ const controller = (function (TodoCtrl, UICtrl) {
           }
         });
       });
+  };
+
+  const showAll = function (todos) {
+    todos.forEach((todo) => {
+      UICtrl.addItemToList(todo);
+    });
   };
 
   const addCompletedClass = function () {
