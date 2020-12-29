5fd7625d066282d4e11843ad25d41e6d44f5eea5
whitegod <kmbagwu12@gmail.com>
2020-12-29 19:09:49 +0100

    React: Working with JSX

--- frontend-mentor.io/ip-address-tracker-master/ip-address-tracker-master/js/models/Map.js
+++ frontend-mentor.io/ip-address-tracker-master/ip-address-tracker-master/js/models/Map.js
@@ -3,29 +3,18 @@ import leaflet from "leaflet";
 const mapContainer = document.querySelector("#map");
 
 export default function generateMap(lng, lat) {
-  // mapboxgl.accessToken =
-  //   "pk.eyJ1Ijoid2hpdGVnb2QiLCJhIjoiY2s5Y3Q5bm52MDdsOTNobzhvenp6MnNsdCJ9.vc6KagoZn4bn0JtaLazS7g";
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoid2hpdGVnb2QiLCJhIjoiY2s5Y3Q5bm52MDdsOTNobzhvenp6MnNsdCJ9.vc6KagoZn4bn0JtaLazS7g";
 
-  // const map = new mapboxgl.Map({
-  //   container: "map",
-  //   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-  //   center: [lng, lat], // starting position [lng, lat]
-  //   zoom: 10, // starting zoom
-  // });
-
-  // const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
-
-  leaflet.map(mapContainer, {
-    center: [lat, lng],
-    zoom: 13,
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+    center: [lng, lat], // starting position [lng, lat]
+    zoom: 11.5, // starting zoom
+    draggable: false,
+    scrollZoom: false,
+    interactive: false,
   });
 
-  leaflet.map(mapContainer).setView([lat, lng], 13);
-
-  leaflet
-    .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    })
-    .addTo(map);
+  const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
 }
--- frontend-mentor.io/static-job-listings-master/static-job-listings-master/static-job-listings/src/index.js
+++ frontend-mentor.io/static-job-listings-master/static-job-listings-master/static-job-listings/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// class App extends Component {
+//   render() {
+//     return <div>Hi There</div>;
+//   }
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const text = ["What is React", "How React Works", "What is JSX"];
+  return (
+    <ul className="list">
+      {text.map((text) => {
+        return (
+          <li className="items">
+            <a href="https" className="link">
+              {text}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
